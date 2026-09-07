131a67132f514c072c5b86ccffb69a30c7c50831
Srushti Ranadive <srushtiranadive4@gmail.com>
2026-09-07 12:50:39 +0530

    Create area.cpp

---
+++ BASICS/area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+float radius, area;
+cout << "Enter the radius:";
+cin >> radius;
+area = 3.14159 * radius * radius;
+
+cout << "Area of the circle = " << area << endl;
+return 0;
+}
