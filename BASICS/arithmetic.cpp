042cb8b5c919a31d72364cd23c639fddf04de479
Srushti Ranadive <srushtiranadive4@gmail.com>
2026-09-07 13:05:27 +0530

    Add arithmetic operations in arithmetic.cpp

---
+++ BASICS/arithmetic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+int a, b;
+
+cin >> a >> b;
+
+cout << "Addition = " << a + b << endl;
+
+cout << "Substraction = " << a - b << endl;
+
+cout << "Multiplication = " << a * b << endl;
+
+cout << "Division = " << a / b << endl;
+
+cout << "Remainder = " << a % b << endl;
+
+return 0;
+
+}
