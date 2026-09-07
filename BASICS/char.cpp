ad8fc4910b90386b65ee6154764783e582e4ca10
Srushti Ranadive <srushtiranadive4@gmail.com>
2026-09-07 12:59:23 +0530

    Create char.cpp

---
+++ BASICS/char.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+char letter, digit, special;
+
+cout << " Enter a letter! ";
+cin >> letter;
+cout << " You entered a letter: " << letter << endl;
+
+cout << "\nEnter a digit: ";
+cin >> digit;
+cout << "You entered a digit: " << digit << endl;
+
+cout << "\Enter a special character: ";
+cin >> special;
+cout << "You entered a special character: " << special << endl;
+
+return 0;
+
+}
